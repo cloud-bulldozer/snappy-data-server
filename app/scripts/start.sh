43adbe305a36c0ace3d3765ee5e3ea14d5af516b
mleader <mleader@redhat.com>
2021-05-26 08:58:07 -0400

    debugging upload large file

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -4,6 +4,7 @@ python $APP_ROOT/app/scripts/backend_prestart.py
 
 uvicorn \
     --host "0.0.0.0" \
-    --log-level $DATA_SERVER_LOG_LVL \
+    # --log-level $DATA_SERVER_LOG_LVL \
+    --log-level debug \
     --port $DATA_SERVER_PORT \
     app.main:app
