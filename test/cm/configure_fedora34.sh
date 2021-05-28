18529eaaafeffbb78f9fcebcd226c9f3febd6d5a
mleader <mleader@redhat.com>
2021-05-28 08:43:27 -0400

    add shell script to configure a fedora box

--- test/cm/configure_fedora34.sh
+++ test/cm/configure_fedora34.sh
@@ -1,30 +1,33 @@
 #! /bin/bash
 
-sudo dnf install -y podman git perl-open.noarch python3-devel gcc libpq-devel python3.9-devel
-git clone https://github.com/mfleader/snappy-data-server.git
-sudo chown -R vagrant snappy-data-server
-sudo chgrp -R vagrant snappy-data-server
-cd snappy-data-server
-git switch streaming
+# sudo dnf install -y podman git perl-open.noarch python3-devel gcc libpq-devel python3.9-devel
+# git clone https://github.com/mfleader/snappy-data-server.git
+# sudo chown -R vagrant snappy-data-server
+# sudo chgrp -R vagrant snappy-data-server
+# cd snappy-data-server
+# git switch streaming
 
 
-export POSTGRES_PASSWORD=secret
-export DATA_SERVER_SECRET=secret
+export POSTGRES_PASSWORD=pass
+export DATA_SERVER_SECRET=pass
 export DATA_SERVER_PUBLIC_HOST=localhost
 export DATA_SERVER_PORT=7070
 export DATA_SERVER_LOG_LVL=info
 export FIRST_SUPERUSER=user@test.com
-export FIRST_SUPERUSER_PASSWORD=secret
+export FIRST_SUPERUSER_PASSWORD=pass
 export POSTGRES_PORT=5432 
 export POSTGRES_SERVER=localhost
-export APP_ROOT=~/snappy-data-server
+# export APP_ROOT=~/snappy-data-server
+export APP_ROOT=$PWD
 export PYTHONPATH=$APP_ROOT/app
 mkdir $APP_ROOT/app/results 
 
 
-python -m pip install wheel
-python -m pip install -r requirements.txt
+# python -m pip install wheel
+# python -m pip install -r requirements.txt
 
+podman container rm pg_svc
+podman volume rm pgvol
 
 podman volume create pgvol
 podman run \
